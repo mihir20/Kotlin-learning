3de3018039f25dd604e317995adb8995200e935a
mihir20 <mihir20121997@gmail.com>
2018-05-27 01:33:38 +0530

    getting current age

--- findMyAge/app/src/main/java/me/mihirgandhi/findmyage/MainActivity.kt
+++ findMyAge/app/src/main/java/me/mihirgandhi/findmyage/MainActivity.kt
@@ -2,11 +2,21 @@ package me.mihirgandhi.findmyage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import me.mihirgandhi.findmyage.R.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(layout.activity_main)
+
+        getAgeButton.setOnClickListener {
+            val birthYear:Int = birthYear.text.toString().toInt()
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+            val yearsOld = (currentYear - birthYear)
+            age.text = "your age is " + yearsOld
+        }
     }
 }
